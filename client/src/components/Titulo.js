60160a69a856d3fd52563bbfbde56dcb413873ce
juliovianadev <juliovianadev@gmail.com>
2023-04-18 16:03:45 -0300

    header criado

---
+++ client/src/components/Titulo.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { ModeloTitulo,TituloData,TituloNome } from '../styles/styles'
+function Titulo() {
+  return (
+    <ModeloTitulo>
+      <TituloNome>Cadastro De tarefas</TituloNome>
+      <TituloData>Terça-Feira, 18 de Abril</TituloData>
+    </ModeloTitulo>
+  )
+}
+
+export default Titulo
