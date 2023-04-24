1095debba588f665442f83aee9dfc89fb0f65c41
juliovianadev <juliovianadev@gmail.com>
2023-04-24 19:03:36 -0300

    criando a rota de procurar o usuario

--- server/routes/LoginRoutes.js
+++ server/routes/LoginRoutes.js
@@ -1,7 +1,24 @@
 const router = require('express').Router();
-
+const jwt = require("jsonwebtoken");
 const LoginController = require("../controllers/LoginController")
+
+function validateToken(req,res,next){
+  const authHeader = req.headers.authorization;
+  const token = authHeader && authHeader.split(" ")[1] 
+  if (!token){
+    return res.status(401).json({message: "Acesso negado! verifique o token"})
+  }
+  try {
+    const secret = process.env.SECRET;
+    jwt.verify(token,secret)
+    next()
+  } catch (error) {
+    return res.status(400).json({message: "Token invalido"})
+  }
+}
+
 
 router.get('/api/login',LoginController.login)
 router.get('/download/image',LoginController.downloadImage)
+router.get('/user/:id',validateToken,LoginController.getUserById)
 module.exports = router;
