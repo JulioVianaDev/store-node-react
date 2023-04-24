f31c280247caf95733fc68a5575c583426618c24
juliovianadev <juliovianadev@gmail.com>
2023-04-24 13:41:39 -0300

    funcionou pois faltava colocar um middleware app.use(express.json());

--- server/app.js
+++ server/app.js
@@ -4,7 +4,7 @@ require('dotenv').config()
 var app = express();
 
 app.use(cors())
-
+app.use(express.json());
 const AuthRegisterUserRoutes = require('./routes/AuthRegisterUserRoutes')
 const port  = process.env.PORT  || 3000
 
